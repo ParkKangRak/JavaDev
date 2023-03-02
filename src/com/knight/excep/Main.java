69930feb9dd06c17155133fd7c4168c7faea2a0d
kangrack333@gmail.com <kangrack333@gmail.com>
2023-03-02 14:40:08 +0900

    arrayList Commit

--- src/com/knight/excep/Main.java
+++ src/com/knight/excep/Main.java
@@ -14,19 +14,19 @@ public class Main {
   public static void main(String[] args) {
     ArrayCalculation arrayCalculation = new ArrayCalculation();
 
-    System.out.println("2 / 1 = " + arrayCalculation.divide(2, 1));
     try {
+      System.out.println("2 / 1 = " + arrayCalculation.divide(2, 1));
       System.out.println(
           "1 / 0 = " + arrayCalculation.divide(1, 0)); // java.lang.ArithmeticException: "/ by zero"
+    } catch (ArithmeticException e) {
+      System.out.println("0으로 나눌수 없습니다. 0을 제외한 값을 넣어주세요");
+    }
+    try {
       System.out.println("Try to divide using out of index element = "
           + arrayCalculation.divide(5, 0)); // java.lang.ArrayIndexOutOfBoundsException: 5
-    } catch (Exception e) {
-      System.out.println("두가지 에러 발생한다.");
-      System.out.println("java.lang.ArithmeticException: \"/ by zero\"");
-      System.out.println("java.lang.ArrayIndexOutOfBoundsException: 5");
+    } catch (ArrayIndexOutOfBoundsException e) {
+      System.out.println("배열의 범위를 초과할수없습니다. "+(arrayCalculation.arr.length-1)+"이하로 넣어주세요");
     }
-
-
   }
 
 }
