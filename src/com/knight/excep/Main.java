69ad1c842a440375fbc2abe816b2f30edf430bb9
kangrack333@gmail.com <kangrack333@gmail.com>
2023-03-02 10:08:44 +0900

    Exception Commit

--- src/com/knight/excep/Main.java
+++ src/com/knight/excep/Main.java
@@ -2,7 +2,7 @@ package com.knight.excep;
 
 class ArrayCalculation {
 
-  int[] arr = { 0, 1, 2, 3, 4 };
+  int[] arr = {0, 1, 2, 3, 4};
 
   public int divide(int denominatorIndex, int numeratorIndex) {
     return arr[denominatorIndex] / arr[numeratorIndex];
@@ -10,13 +10,23 @@ class ArrayCalculation {
 }
 
 public class Main {
+
   public static void main(String[] args) {
     ArrayCalculation arrayCalculation = new ArrayCalculation();
 
     System.out.println("2 / 1 = " + arrayCalculation.divide(2, 1));
-    System.out.println("1 / 0 = " + arrayCalculation.divide(1, 0)); // java.lang.ArithmeticException: "/ by zero"
-    System.out.println("Try to divide using out of index element = "
-        + arrayCalculation.divide(5, 0)); // java.lang.ArrayIndexOutOfBoundsException: 5
+    try {
+      System.out.println(
+          "1 / 0 = " + arrayCalculation.divide(1, 0)); // java.lang.ArithmeticException: "/ by zero"
+      System.out.println("Try to divide using out of index element = "
+          + arrayCalculation.divide(5, 0)); // java.lang.ArrayIndexOutOfBoundsException: 5
+    } catch (Exception e) {
+      System.out.println("두가지 에러 발생한다.");
+      System.out.println("java.lang.ArithmeticException: \"/ by zero\"");
+      System.out.println("java.lang.ArrayIndexOutOfBoundsException: 5");
+    }
+
+
   }
 
 }
